5c86e1d938e1ceff448e5b6ddbf6be8d0d1828c9
PratikBalaji <60303726+PratikBalaji@users.noreply.github.com>
2020-09-13 13:53:37 -0400

    added catapult and rubber band to the angry bird game

--- Slingshot.js
+++ Slingshot.js
@@ -1,13 +1,16 @@
 class SlingShot{
     constructor(bodyA, pointB){
-        var options = {
+        this.sling1=loadImage("sprites/sling1.png");//loads the image
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
+        var options = {//creates the stiffness and length for the slingshot
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
         this.pointB = pointB
-        this.sling = Constraint.create(options);
+        this.sling = Constraint.create(options);//creates a Constraint to hold the bird in a certain area
         World.add(world, this.sling);
     }
 
@@ -16,11 +19,26 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,20);//creates image for the Catapult 1 in the right side
+        image(this.sling2,170,20);//creates image for the catapult 2 in the left side
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
+            push();
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke("#301708");//gives the same brownish color to the rubber band
+            if(pointA.x<220){
+                strokeWeight(7);//Increases the thickness of the rubber band 
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20,pointA.y,pointB.x+30,pointB.y);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            }
+            else {
+                strokeWeight(4);//decreases the thicness of the rubber band
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25,pointA.y,pointB.x+30,pointB.y);
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);//creates the rubber band ande places the seat for the bird in the front and back of the bird
+            }
+            pop();
         }
     }
     
